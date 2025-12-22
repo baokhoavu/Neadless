48c6027457ad01411de691b283329b1bb620ae80
baokhoavu <baokhoadinhvu@gmail.com>
2025-12-22 12:21:01 -0800

    Update Homepage further with labels

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,9 @@ function Intro() {
 	return (
 		<section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
 			<h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-				Blog.
 			</h1>
 			<h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-				A statically generated blog example using{" "}
+				<strong>Neadless.</strong> A headless CMS full stack Web Application using{" "}
 				<a
 					href="https://nextjs.org/"
 					className="underline hover:text-success duration-200 transition-colors"
